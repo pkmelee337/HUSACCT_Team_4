276d00be680b07a6a81348a1bc53009a54fc0e07
Henk ter Harmsel <henkterharmsel92@gmail.com>
2012-04-02 21:53:13 +0200

    Added GUI Team 3 - now getting it to work

---
+++ src/define/view_control/helper/DataHelper.java
@@ -0,0 +1,36 @@
+package define.view_control.helper;
+
+public class DataHelper {
+	private int intid;
+	private long longid;
+	private String value;
+
+	public void setId(int id) {
+		this.intid = id;
+	}
+
+	public void setId(long id) {
+		this.longid = id;
+	}
+
+	public int getIntId() {		
+		return intid;
+	}
+	
+	public long getLongId(){
+		return longid;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public String toString() {
+		return getValue();
+	}
+
+}
